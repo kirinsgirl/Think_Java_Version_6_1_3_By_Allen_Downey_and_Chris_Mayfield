cca79b248062298e5e29810d231f089e3792baee
kirinsgirl <nahdahhanifah@proton.email>
2026-03-02 10:11:35 +0700

    exercise_2

---
+++ chapter_3/exercise_2/exc_2.java
@@ -0,0 +1,25 @@
+/*
+    This code aims to convert Celcius degree to Fahrenheit degree
+*/
+
+
+package chapter_3.exercise_2;
+import java.util.Scanner;
+
+public class exc_2 
+{
+    public static void main(String[] args)
+    {
+        double celcius;
+        double fahrenheit;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a temperature in Celcius: ");
+        celcius = in.nextDouble();
+
+        double fahrenheit_converter = (celcius * 1.8) + 32;
+        fahrenheit = fahrenheit_converter;
+        System.out.println(celcius + " C" + " = " + fahrenheit + " F.");
+    }
+}
+
