66c4b42f7bf35a105a16c7b37db4fafa2c5b0090
kirinsgirl <nahdahhanifah@proton.email>
2026-03-10 14:59:43 +0700

    exercise

---
+++ chapter_5/exercise_7/GuessingNumber2.java
@@ -0,0 +1,50 @@
+package chapter_5.exercise_7;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessingNumber2 
+{
+    public static void guessedNumber(int randomNumber, int inputNumber)
+    {
+
+        if (inputNumber == randomNumber) {
+            System.out.println("You're correct!");
+        } else {
+            if (inputNumber > randomNumber) {
+                System.out.println("Too high.");
+                System.out.print("Take another guess: ");
+                Scanner in = new Scanner(System.in);
+                inputNumber = in.nextInt();
+                guessedNumber(randomNumber, inputNumber);
+            } else {
+                System.out.println("Too low.");
+                System.out.print("Take another guess: ");
+                Scanner in = new Scanner(System.in);
+                inputNumber = in.nextInt();
+                guessedNumber(randomNumber, inputNumber);
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args)    
+    {
+        System.out.println("I'm thinking of a number between 1 and 100" +
+        " (including both). Can you guess what is it?");
+        
+        Scanner in = new Scanner(System.in);
+        System.out.println("Type a number: ");
+
+        int inputNumber;
+        inputNumber = in.nextInt();
+        System.out.println("Your guess is " + inputNumber);
+
+        Random random = new Random();
+        int randomNumber;
+        randomNumber = random.nextInt(100) + 1;
+
+        guessedNumber(randomNumber, inputNumber);
+    }
+}
